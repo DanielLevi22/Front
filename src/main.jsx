de606522d1d2d7baef8c6c11b32375e17473d988
Daniel Levi <daniellevi000@gmail.com>
2023-02-18 15:34:23 -0300

    correcao

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme';
-import { Home } from './pages/Home';
-import { SingIn } from './pages/SignIn';
-import { SingUp } from './pages/SignUp';
-import { Profile } from './pages/Profile';
 import { AuthProvider } from './hooks/auth';
 import { Routes } from './routes';
 
